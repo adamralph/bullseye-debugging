1b4a0b0f18883d5193210b1339e45441ae50718e
Adam Ralph <adam@adamralph.com>
2019-03-17 08:46:12 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static Bullseye.Targets;
+
+namespace bullseye_debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+            {
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+            };
+
+            Target("default", () => Console.WriteLine("Hello World!"));
+
+            RunTargetsAndExit(args);
+        }
+    }
+}
